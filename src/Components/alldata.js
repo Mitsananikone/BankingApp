0cfa0277199e02350a7911f8857c00795ce2a33a
Mit Sananikone <Mitsananikone@gmail.com>
2022-11-18 13:13:55 -0600

    AllData showing bootstrap table and ALL users

--- src/Components/alldata.js
+++ src/Components/alldata.js
@@ -1,17 +1,85 @@
 import React from 'react';
-import {UserContext} from "./context";
+import {UserContext, UserChart} from "./context";
+import {Table} from 'react-bootstrap/Table';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import Table from "react-bootstrap-table-next";
 
 
 function AllData(){
     const ctx = React.useContext(UserContext);
-    return (
-      <>
-      <h5>All Data in Store</h5>
-        <div>
-        {JSON.stringify(ctx.users)}
-        </div>
-      </>
+    const currentUser = ctx.users.length-1;
+
+    const addTableRow = (result) => {
+      return (
+        <tr key={result}>
+          <td>{result.name}</td>
+          <td>{result.email}</td>
+          <td>{result.password}</td>
+       
+        
+        </tr>)
+    }
+  
+    const createTable = (results) => {
+
+
+      return (
+        <table className="striped highlight ">
+          <thead>
+            <tr>
+            
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
+    
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((result, index) => {
+              return addTableRow(result)
+            })}
+          </tbody>
+        </table>
+      )
+    }
+
+      return (
+      <Table
+      txtcolor="black"
+      header="All Data"
+
+
+      body={ 
+        <>
+        {createTable(ctx.users)}
+        
+        </>
+      // {JSON.stringify(ctx.users)}
+        // <Table striped bordered hover>
+        //   <thead>
+        //     <tr>
+        //       <th>ID</th>
+        //       <th>Name</th>
+        //       <th>Email</th>
+        //       <th>Password</th>
+        //       <th>Balance</th>
+        //     </tr>
+        //   </thead>
+        // <tbody>
+        //   <td> {id} </td>
+        //   <td> {name} </td>
+        //   <td> {email} </td>
+        //   <td> {password} </td>
+        //   <td> {balance} </td>
+        // </tbody>
+        // </Table>
+
+       
+    
+      }       
+    />    
     );
-  }
+  };
 
 export default AllData;
--- src/Components/context.js
+++ src/Components/context.js
@@ -1,6 +1,8 @@
 import React, { useState }from "react";
 import Button from 'react-bootstrap/Button';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import Table from "react-bootstrap-table-next";
 
 const UserContext = React.createContext(null
   // {users:[{id: 0, name:'Mit',email:'Mitsananikone@gmail.com',password:'secret',balance:1000}]}
@@ -47,10 +49,31 @@ function Card(props){
     );
   }
 
+  function Table(props) {
+    function classes(){
+      const bg  = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
+      const txt = props.txtcolor ? ' text-' + props.txtcolor: ' text-white';
+      return 'card mb-3 ' + bg + txt;
+    }
+
+    return (
+      <div className={classes()} style={{maxWidth: "50VW"}}>
+       <div className="card-header">{props.header}</div>
+       <div className="card-body" >
+         {/* {props.title && (<h5 className="card-title">{props.title}</h5>)} */}
+         {/* {props.input && (<input type="input" className="form-control" id="submit">{props.input}</input>)} */}
+         {props.body}
+   
+         
+       </div>
+     </div>      
+   );
+  }
 
 
 
 
 
 
-export {UserContext, Card, ATM};
+
+export {UserContext, Card, UserChart, ATM};
